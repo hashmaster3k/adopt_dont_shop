cacc84af9e13e90da64498e8854a7c5413c5d91e
Hashim Gari <hashmaster3k@gmail.com>
2020-08-19 17:29:13 -0600

    Remove before do block due to errors

--- spec/features/shelters/show_pets_spec.rb
+++ spec/features/shelters/show_pets_spec.rb
@@ -3,20 +3,21 @@
 require 'rails_helper'
 
 RSpec.describe 'shelter show pets' do
-  before do
-    @shelter_1 = Shelter.create(name: 'Shelter 1',
+
+  it 'shows only adoptable pets and attributes' do
+    shelter_1 = Shelter.create(name: 'Shelter 1',
                                 address: '123 Bradford Rd',
                                 city: 'Union City',
                                 state: 'CA',
                                 zip: 90210)
 
-    @pet_1 =  Pet.create(image: '1.jpg',
+    pet_1 =  Pet.create(image: '1.jpg',
                          name: 'Johnny',
                          approx_age: 3,
                          sex: 'male',
                          description: 'A very nice pet!',
                          adopted: false,
-                         shelter_id: @shelter_1.id)
+                         shelter_id: shelter_1.id)
 
     @pet_2 =  Pet.create(image: '2.jpg',
                          name: 'Maggie',
@@ -24,18 +25,24 @@ RSpec.describe 'shelter show pets' do
                          sex: 'female',
                          description: 'A great pet!',
                          adopted: true,
-                         shelter_id: @shelter_1.id)
+                         shelter_id: shelter_1.id)
 
-    visit "/shelters/#{@shelter_1.id}/pets"
-  end
+    visit "/shelters/#{shelter_1.id}/pets"
 
-  it 'shows only adoptable pets and attributes' do
-    expect(page).to have_content(@pet_1.name)
-    expect(page).to have_content(@pet_1.approx_age)
-    expect(page).to have_content(@pet_1.sex)
+    expect(page).to have_content(pet_1.name)
+    expect(page).to have_content(pet_1.approx_age)
+    expect(page).to have_content(pet_1.sex)
   end
 
   it 'has link to create new pet' do
+    shelter_1 = Shelter.create(name: 'Shelter 1',
+                                address: '123 Bradford Rd',
+                                city: 'Union City',
+                                state: 'CA',
+                                zip: 90210)
+
+    visit "/shelters/#{shelter_1.id}/pets"
+
     expect(page).to have_link('Add new pet to shelter')
   end
 end
--- spec/features/shelters/show_spec.rb
+++ spec/features/shelters/show_spec.rb
@@ -3,39 +3,50 @@
 require 'rails_helper'
 
 RSpec.describe 'shelter show page' do
-  before do
-    @shelter_1 = Shelter.create(name: 'Shelter 1',
+
+  it 'shows the shelters name, address, city, state and zip' do
+    shelter_1 = Shelter.create(name: 'Shelter 1',
                                 address: '123 Bradford Rd',
                                 city: 'Union City',
                                 state: 'CA',
                                 zip: 90210)
-  end
 
-  it 'shows the shelters name, address, city, state and zip' do
+    visit "/shelters/#{shelter_1.id}"
 
-    visit "/shelters/#{@shelter_1.id}"
-
-    expect(page).to have_content(@shelter_1.name)
-    expect(page).to have_content(@shelter_1.address)
-    expect(page).to have_content(@shelter_1.city)
-    expect(page).to have_content(@shelter_1.state)
-    expect(page).to have_content(@shelter_1.zip)
+    expect(page).to have_content(shelter_1.name)
+    expect(page).to have_content(shelter_1.address)
+    expect(page).to have_content(shelter_1.city)
+    expect(page).to have_content(shelter_1.state)
+    expect(page).to have_content(shelter_1.zip)
   end
 
   it 'has a link to update shelter info' do
-    visit "/shelters/#{@shelter_1.id}"
+    shelter_1 = Shelter.create(name: 'Shelter 1',
+                                address: '123 Bradford Rd',
+                                city: 'Union City',
+                                state: 'CA',
+                                zip: 90210)
+
+    visit "/shelters/#{shelter_1.id}"
 
     expect(page).to have_link('Update shelter')
 
     click_link "Update shelter"
-    expect(current_path).to eq("/shelters/#{@shelter_1.id}/edit")
+    expect(current_path).to eq("/shelters/#{shelter_1.id}/edit")
   end
 
   it 'has a link to delete shelter' do
-    visit "/shelters/#{@shelter_1.id}"
+    shelter_1 = Shelter.create(name: 'Shelter 1',
+                                address: '123 Bradford Rd',
+                                city: 'Union City',
+                                state: 'CA',
+                                zip: 90210)
+
+    visit "/shelters/#{shelter_1.id}"
     expect(page).to have_button('delete shelter')
 
     click_button "delete shelter"
+
     expect(Shelter.all.length).to eq(0)
     expect(current_path).to eq('/shelters')
   end
