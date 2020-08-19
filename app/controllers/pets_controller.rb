74994ba1dfdcff711b0cf14a2aedaf5182342bdf
Hashim Gari <hashmaster3k@gmail.com>
2020-08-19 17:30:18 -0600

    Adds create pet to shelter

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -7,4 +7,16 @@ class PetsController < ApplicationController
   def show
     @pet = Pet.find(params[:id])
   end
+
+  def create
+    pet = Pet.new(name: params[:pet][:name],
+                  approx_age: params[:pet][:approx_age],
+                  sex: params[:pet][:sex],
+                  image: params[:pet][:image],
+                  description: params[:pet][:description],
+                  shelter_id: params[:pet][:shelter_id],
+                  adopted: false)
+    pet.save
+    redirect_to "/shelters/#{params[:id]}/pets"
+  end
 end
