9f8c4ed69387c879dec6db396de02bbf7e671e81
Hashim Gari <hashmaster3k@gmail.com>
2020-08-21 08:55:58 -0600

    Update test to account for change links

--- spec/features/shelters/show_spec.rb
+++ spec/features/shelters/show_spec.rb
@@ -29,9 +29,8 @@ RSpec.describe 'shelter show page' do
 
     visit "/shelters/#{shelter_1.id}"
 
-    expect(page).to have_link('Update shelter')
-
-    click_link "Update shelter"
+    expect(page).to have_button('Update Shelter')
+    click_button "Update Shelter"
     expect(current_path).to eq("/shelters/#{shelter_1.id}/edit")
   end
 
@@ -43,9 +42,9 @@ RSpec.describe 'shelter show page' do
                                 zip: 90210)
 
     visit "/shelters/#{shelter_1.id}"
-    expect(page).to have_button('delete shelter')
+    expect(page).to have_button('Delete Shelter')
 
-    click_button "delete shelter"
+    click_button "Delete Shelter"
 
     expect(current_path).to eq('/shelters')
   end
