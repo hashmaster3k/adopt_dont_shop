d48a10f3ccd34ee16e3a2a6e04fdd3c1a372841b
Hashim Gari <hashmaster3k@gmail.com>
2020-08-25 16:34:54 -0600

    Creates link to non-setup page for favorite pets

--- spec/features/layouts/application_spec.rb
+++ spec/features/layouts/application_spec.rb
@@ -29,4 +29,15 @@
      expect(page).to have_link('SHELTERS', visible: false)
      expect(page).to have_link('PETS', visible: false)
    end
+
+   it 'has a favorite pets counter visible from any page' do
+     visit '/'
+     expect(page).to have_link('FAVORITE PETS:', visible: false)
+     click_link 'HOME', visible: false
+     expect(page).to have_link('FAVORITE PETS:', visible: false)
+     click_link 'SHELTERS', visible: false
+     expect(page).to have_link('FAVORITE PETS:', visible: false)
+     click_link 'PETS', visible: false
+     expect(page).to have_link('FAVORITE PETS:', visible: false)
+   end
  end
