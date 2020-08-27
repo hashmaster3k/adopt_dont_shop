f3bc814720fa3ce47670680d57cc8ddfe83b09f3
Hashim Gari <hashmaster3k@gmail.com>
2020-08-27 15:14:16 -0600

    Adds text for no favorites added

--- spec/features/favorites/index_spec.rb
+++ spec/features/favorites/index_spec.rb
@@ -4,6 +4,30 @@ require 'rails_helper'
 
 RSpec.describe 'index favorites page' do
 
+  before :each do
+    shelter_1 = Shelter.create(name:'Shelter 1',
+                               address:'123 Bradford Rd',
+                               city:'Union City',
+                               state:'CA',
+                               zip:90210)
+
+
+    @pet_1 = shelter_1.pets.create(image: 'dog1.jpg',
+                       name: 'Johnny',
+                       approx_age: 3,
+                       sex: 'male',
+                       shelter_id: shelter_1.id)
+
+    @pet_2 = shelter_1.pets.create(image: 'dog2.jpg',
+                       name: 'Marg',
+                       approx_age: 2,
+                       sex: 'female',
+                       shelter_id: shelter_1.id)
+
+    Favorite.create(pet_id: @pet_1.id)
+    Favorite.create(pet_id: @pet_2.id)
+  end
+
   it 'has link to favorites page and can visit it' do
     visit '/'
     expect(page).to have_link('FAVORITE PETS:', visible: false)
@@ -12,34 +36,18 @@ RSpec.describe 'index favorites page' do
   end
 
   it 'displays all favorite pets' do
-    shelter_1 = Shelter.create(name:'Shelter 1',
-                               address:'123 Bradford Rd',
-                               city:'Union City',
-                               state:'CA',
-                               zip:90210)
-
-
-    pet_1 = shelter_1.pets.create(image: 'dog1.jpg',
-                       name: 'Johnny',
-                       approx_age: 3,
-                       sex: 'male',
-                       shelter_id: shelter_1.id)
-
-    pet_2 = shelter_1.pets.create(image: 'dog2.jpg',
-                       name: 'Marg',
-                       approx_age: 2,
-                       sex: 'female',
-                       shelter_id: shelter_1.id)
-
-    Favorite.create(pet_id: pet_1.id)
-    Favorite.create(pet_id: pet_2.id)
-
     visit '/favorites'
 
-    expect(page).to have_content(pet_1.name)
-    expect(page).to have_content(pet_2.name)
+    expect(page).to have_content(@pet_1.name)
+    expect(page).to have_content(@pet_2.name)
 
-    visit "/pets/#{pet_1.id}"
+    visit "/pets/#{@pet_1.id}"
+  end
+
+  it 'displays message when no favorites added' do
+    visit '/favorites'
+
+    expect(page).to have_content('No favorited pets :( Please add some!')
   end
 
 end
