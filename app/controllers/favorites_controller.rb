18becff4a01e92d6c6ed2fa6306f9f5d52ead697
James Belta <james.belta@gmail.com>
2020-08-27 14:48:35 -0600

    Build destroy action

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,6 +4,13 @@ class FavoritesController < ApplicationController
     @pets = Favorite.find_by_id
   end
 
+  def destroy
+    binding.pry
+    favorite = Favorite.where(pet_id: params[:pet_id]).first
+    Favorite.destroy(favorite.id)
+    redirect_to '/favorites'
+  end
+
   def create
     Favorite.create(favorite_params)
     flash[:notice] = "This Pet has been Added to your Favorites"
