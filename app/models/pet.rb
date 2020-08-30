29080209f7d79790b6567c1d04e0b167d0faaec4
James Belta <james.belta@gmail.com>
2020-08-29 23:41:06 -0600

    needed validations?

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -3,6 +3,13 @@ class Pet < ApplicationRecord
   has_many :application_pets
   has_many :applications, through: :application_pets
 
+  #validates_presence_of(:name)
+  #validates_presence_of(:approx_age)
+  #validates_presence_of(:sex)
+  #validates_presence_of(:image)
+  #validates_presence_of(:shelter_id)
+  #validates_presence_of(:description)
+
   def self.find_pets_by_ids(array_ids)
     Pet.where(id: array_ids)
   end
